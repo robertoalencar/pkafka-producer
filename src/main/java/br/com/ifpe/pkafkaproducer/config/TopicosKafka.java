fe2d9d7ac605fb3589445e637e4c534037c09bf4
Roberto Alencar <rroberto.alencar@gmai.com>
2022-11-01 09:18:12 -0300

    implementando o producer

---
+++ src/main/java/br/com/ifpe/pkafkaproducer/config/TopicosKafka.java
@@ -0,0 +1,7 @@
+package br.com.ifpe.pkafkaproducer.config;
+
+public class TopicosKafka {
+
+    public static final String TOPICO_TESTE = "teste-topico";
+
+}
---
+++ src/main/java/br/com/ifpe/pkafkaproducer/servicos/ProducerService.java
@@ -0,0 +1,17 @@
+package br.com.ifpe.pkafkaproducer.servicos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProducerService {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topico, String message) {
+
+	kafkaTemplate.send(topico, message);
+    }
+}
